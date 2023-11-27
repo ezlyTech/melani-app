dc3d13fc7b2c8897ecd12a2da94299a1c431b039
Eliz Marie Manalo <tech.ezly@gmail.com>
2023-11-27 16:18:34 +0800

    server updates

--- server/app.js
+++ server/app.js
@@ -1,24 +1,27 @@
-import express from "express"
-import cors from "cors"
-import dotenv from "dotenv"
-import { initDatabase } from "./database.js"
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+// eslint-disable-next-line import/extensions
+import { initDatabase } from "./database.js";
 
-const app = express()
-dotenv.config()
-initDatabase()
+const app = express();
+dotenv.config();
+initDatabase();
 
-const port = 3031
+const port = 3031;
 
 // MIDDLEWARE
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(cors({
-  origin: "http://localhost:3030",
-  credentials: true,
-}))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3030",
+    credentials: true,
+  })
+);
 
-app.listen(port, console.log(`app is running \nListening on port ${port}`))
+app.listen(port, console.log(`app is running \nListening on port ${port}`));
 
 app.get("/", (req, res) => {
-  res.send("test")
-})
+  res.send("test");
+});
--- server/database.js
+++ server/database.js
@@ -1,8 +1,9 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import mongoose from "mongoose";
 
 export function initDatabase() {
-  mongoose.connect(process.env.VITE_DATABASE_URI)
-  const db = mongoose.connection
-  db.on("error", (error) => console.error(error))
-  db.once("open", () => console.log("CONNECTED TO THE DATABASE"))
+  mongoose.connect(process.env.VITE_DATABASE_URI);
+  const db = mongoose.connection;
+  db.on("error", (error) => console.error(error));
+  db.once("open", () => console.log("CONNECTED TO THE DATABASE"));
 }
