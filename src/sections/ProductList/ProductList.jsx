ab727a6e8f48ceb8a2b2a15474cb63bcd741480b
marc <justineolimverio@gmail.com>
2023-11-29 12:24:56 +0800

    Added sample cards on product list page

--- src/sections/ProductList/ProductList.jsx
+++ src/sections/ProductList/ProductList.jsx
@@ -1,17 +1,63 @@
 import {
   Container,
-  Typography,
+  Grid,
 } from "@mui/material";
+import { TitleTypography, ProductCard } from "src/components";
+
 
 
 const ProductList = () => {
-  const test = "Product Lists";
+  const sampleCategoryName = "Signature Dishes";
+  const sampleProducts = [
+    {
+      name: "Chocolate Obscura",
+      image: "/assets/images/products/1.png",
+      price: "154.00",
+      rating: 4,
+    },
+    {
+      name: "Biscuit Munch",
+      image: "/assets/images/products/2.png",
+      price: "154.00",
+      rating: 3,
+    },
+    {
+      name: "Alfredo Penne",
+      image: "/assets/images/products/3.png",
+      price: "154.00",
+      rating: 5,
+    },
+    {
+      name: "Cinnamon Rolls",
+      image: "/assets/images/products/4.png",
+      price: "154.00",
+      rating: 5,
+    },
+    {
+      name: "Velvet Eclipse",
+      image: "/assets/images/products/5.png",
+      price: "154.00",
+      rating: 5,
+    },
+    {
+      name: "Strawberry Cake",
+      image: "/assets/images/products/6.png",
+      price: "154.00",
+      rating: 5,
+    },
+  ]
 
   return (
     <Container>
-      <Typography sx={{ mb: 5 }}>
-        {test}
-      </Typography>
+      <TitleTypography value={sampleCategoryName} />
+
+      <Grid container spacing={2}>
+        {sampleProducts.map((product) => (
+          <Grid item xs key={product.key}>
+            <ProductCard product={product} />
+          </Grid>
+        ))}
+      </Grid>
     </Container >
   )
 }
