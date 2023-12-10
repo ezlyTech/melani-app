288cb7e6ff962888146e4be4259531c986914e9e
Eliz Marie Manalo <tech.ezly@gmail.com>
2023-12-10 15:02:02 +0800

    Added welcome message in home

--- src/sections/home/home.jsx
+++ src/sections/home/home.jsx
@@ -1,13 +1,25 @@
 import {
   Card,
   CardMedia,
-  Container
+  Container,
+  Typography
 } from "@mui/material";
 import { HomeCategoriesBlock, HomeMenuBlock } from "./components";
 
 export default function Home() {
+  const name = "John";
+
   return (
     <>
+      <Container sx={{ bgcolor: "#FFEEE1" }}>
+        <Typography
+          mt={1} mb={1}
+          variant="h4"
+          color="#3D2209"
+        >
+          Welcome, {name}!
+        </Typography>
+      </Container>
       <Card sx={{ borderRadius: 0 }}>
         <CardMedia
           sx={{ height: 140 }}
