5075414de623701e2c1640e3d7c6755fa8bf5014
jmagwili <joshuamagwili@gmail.com>
2023-12-11 20:21:29 +0800

    added get method for single item at item route

--- server/routes/itemRoute.js
+++ server/routes/itemRoute.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import axios from 'axios'
+import { JSDOM } from 'jsdom'
 
 const itemRoute = express.Router()
 const token = "c444dac47f47470bb0ee9ddf4213fa75"
 
-itemRoute.get('/:categoryID', async (req, res) => {
+itemRoute.get('/list/:categoryID', async (req, res) => {
   try {
     const itemData = await axios.get("https://api.loyverse.com/v1.0/items", {
       headers: {
@@ -28,6 +29,61 @@ itemRoute.get('/:categoryID', async (req, res) => {
 
     console.log("successfully fetched items")
     res.send(items)
+  } catch (err) {
+    console.log(err)
+  }
+})
+
+itemRoute.get('/single/:productID', async (req, res) => {
+  try {
+    const itemData = await axios.get(`https://api.loyverse.com/v1.0/items/${req.params.productID}`, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    })
+    // Create a DOM from the HTML content
+    const dom = new JSDOM(itemData.data.description);
+    const paragraphs = dom.window.document.querySelectorAll("p");
+
+    // Remove <p> tags
+    paragraphs.forEach((p) => {
+      const parent = p.parentNode;
+      while (p.firstChild) {
+        parent.insertBefore(p.firstChild, p);
+      }
+      parent.removeChild(p);
+    });
+
+    console.log("successfully fetched an item")
+
+    res.send({
+      name: itemData.data.item_name,
+      price: itemData.data.variants[0].default_price,
+      image: itemData.data.image_url,
+      rating: 4,
+      information: dom.window.document.body.innerHTML,
+      option: ["slice", "whole"],
+      uploads: [
+        {
+          url: "/assets/images/products/1.png",
+        },
+        {
+          url: "/assets/images/products/2.png",
+        },
+        {
+          url: "/assets/images/products/3.png",
+        },
+        {
+          url: "/assets/images/products/4.png",
+        },
+        {
+          url: "/assets/images/products/5.png",
+        },
+        {
+          url: "/assets/images/products/6.png",
+        },
+      ],
+    })
   } catch (err) {
     console.log(err)
   }
