e00fc6ce5c905eef3a9fcb713fe0572eb0af65b6
jmagwili <joshuamagwili@gmail.com>
2023-12-11 17:33:02 +0800

    updated categories route at server

--- server/routes/categoryRoute.js
+++ server/routes/categoryRoute.js
@@ -1,11 +1,11 @@
 import express from "express";
 import axios from "axios";
 
-const categories = express.Router()
+const categoryRoute = express.Router()
 
 const token = "c444dac47f47470bb0ee9ddf4213fa75"
 
-categories.get('/', async (req, res) => {
+categoryRoute.get('/', async (req, res) => {
   let categories = []
   try {
     const itemData = await axios.get("https://api.loyverse.com/v1.0/categories", {
@@ -29,4 +29,4 @@ categories.get('/', async (req, res) => {
 
 })
 
-export default categories
+export default categoryRoute
