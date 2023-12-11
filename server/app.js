a2a246fe92eb7a4e55e6392bdd68a00d61c5275b
jmagwili <joshuamagwili@gmail.com>
2023-12-11 16:08:21 +0800

    added categories route at server

--- server/app.js
+++ server/app.js
@@ -1,14 +1,17 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import axios from "axios";
 // eslint-disable-next-line import/extensions
 import { initDatabase } from "./database.js";
+import categories from "./routes/categories.js";
 
 const app = express();
 dotenv.config();
 initDatabase();
 
 const port = 3031;
+const token = "c444dac47f47470bb0ee9ddf4213fa75"
 
 // MIDDLEWARE
 app.use(express.urlencoded({ extended: true }));
@@ -25,3 +28,22 @@ app.listen(port, console.log(`app is running \nListening on port ${port}`));
 app.get("/", (req, res) => {
   res.send("test");
 });
+
+app.get('/fetch-data', async (req, res) => {
+  try {
+    const itemData = await axios.get("https://api.loyverse.com/v1.0/categories", {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    })
+
+    console.log(itemData.data)
+
+  } catch (err) {
+    console.log(err)
+  }
+
+  res.send('ok')
+})
+
+app.use('/test', categories)
---
+++ server/routes/categories.js
@@ -0,0 +1,23 @@
+import express from "express";
+import axios from "axios";
+
+const categories = express.Router()
+
+const token = "c444dac47f47470bb0ee9ddf4213fa75"
+
+categories.get('/', async (req, res) => {
+  try {
+    const itemData = await axios.get("https://api.loyverse.com/v1.0/categories", {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    })
+    console.log("successfully fetched categories")
+    res.send(itemData.data)
+  } catch (err) {
+    console.log(err)
+  }
+
+})
+
+export default categories
