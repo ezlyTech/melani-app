203b439466a3a7835d0130b89e54ce0245fdd9b3
jmagwili <joshuamagwili@gmail.com>
2023-12-11 18:29:56 +0800

    updated the get method at item route

--- server/routes/itemRoute.js
+++ server/routes/itemRoute.js
@@ -1,9 +1,35 @@
 import express from 'express'
+import axios from 'axios'
 
 const itemRoute = express.Router()
+const token = "c444dac47f47470bb0ee9ddf4213fa75"
 
-itemRoute.get('/', (req, res) => {
-  res.send({ message: "it works" })
+itemRoute.get('/:categoryID', async (req, res) => {
+  try {
+    const itemData = await axios.get("https://api.loyverse.com/v1.0/items", {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    })
+
+    let items = []
+
+    for (let i in itemData.data.items) {
+      if (itemData.data.items[i].category_id === req.params.categoryID) {
+        items.push({
+          name: itemData.data.items[i].item_name,
+          image: itemData.data.items[i].image_url,
+          price: itemData.data.items[i].variants[0].default_price,
+          rating: 4
+        })
+      }
+    }
+
+    console.log("successfully fetched items")
+    res.send(items)
+  } catch (err) {
+    console.log(err)
+  }
 })
 
 export default itemRoute
