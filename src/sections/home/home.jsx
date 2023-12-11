695ea71745742cdcee8f5d7d25f6678eabdeb8a1
jmagwili <joshuamagwili@gmail.com>
2023-12-11 16:16:44 +0800

    added hook to fetch data from server

--- src/sections/home/home.jsx
+++ src/sections/home/home.jsx
@@ -4,10 +4,31 @@ import {
   Container,
   Typography
 } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { HomeCategoriesBlock, HomeMenuBlock } from "./components";
 
 export default function Home() {
   const name = "John";
+  const [categories, setCategories] = useState()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const itemData = await axios.get("http://localhost:3031/api/categories")
+        setCategories(itemData.data)
+
+      } catch (err) {
+        console.log(err)
+      }
+
+    }
+    fetchData()
+  }, [])
+
+  useEffect(() => {
+    console.log("categories: ", categories)
+  }, [categories])
 
   return (
     <>
