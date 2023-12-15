ec726b6e95ead5dc41c4a97e63a6f79f8e0e5ab0
marc <justineolimverio@gmail.com>
2023-12-15 17:31:17 +0800

    Update Review.jsx

--- src/sections/Review/Review.jsx
+++ src/sections/Review/Review.jsx
@@ -7,6 +7,8 @@ import {
   Container,
   Box,
   Rating,
+  TextField,
+  Button,
 } from "@mui/material"
 
 import { useState } from "react";
@@ -21,14 +23,19 @@ const Review = () => {
 
   return (
     <Container>
-      <Accordion expanded={expanded === "panel1"} onChange={handleChange("panel1")} sx={{ backgroundColor: "#FFFAF6", borderRadius: 15 }}>
+      <Accordion expanded={expanded === "panel1"} onChange={handleChange("panel1")}
+        sx={{
+          backgroundColor: "#FFFAF6",
+          borderRadius: "15px",
+          marginBottom: "8px"
+        }}>
         <AccordionSummary
           //  expandIcon={<ExpandMoreIcon />}
           aria-controls="panel1bh-content"
           id="panel1bh-header"
         >
           <Box sx={{ display: "flex", gap: "1em" }}>
-            <img src="/assests/images/products/2.png"
+            <img src="/assets/images/products/2.png"
               alt=""
               style={{
                 height: "75px",
@@ -44,21 +51,31 @@ const Review = () => {
 
         <AccordionDetails>
           <Typography>
-            Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat.
-            Aliquam eget maximus est, id dignissim quam.
+            <TextField id="outlined-basic" label="Write a review" variant="outlined" sx={{ width: "100%" }} />
           </Typography>
+
         </AccordionDetails>
       </Accordion>
 
 
-      <Accordion expanded={expanded === "panel2"} onChange={handleChange("panel2")} sx={{ backgroundColor: "#FFFAF6" }}>
+      <Accordion expanded={expanded === "panel2"} onChange={handleChange("panel2")}
+        sx={{
+          backgroundColor: "#FFFAF6",
+          borderRadius: "15px",
+          marginBottom: "8px"
+        }}>
         <AccordionSummary
           //  expandIcon={<ExpandMoreIcon />}
           aria-controls="panel2bh-content"
           id="panel2bh-header"
         >
           <Box sx={{ display: "flex", gap: "1em" }}>
-            <img src="/assests/images/products/2.png" alt="" style={{ height: "75px", width: "106px", objectFit: "cover" }} />
+            <img src="/assets/images/products/2.png" alt=""
+              style={{
+                height: "75px",
+                width: "106px",
+                objectFit: "cover"
+              }} />
             <Box>
               <Typography sx={{ fontWeight: "bold" }}>Biscuit Munch</Typography>
               <Typography>1x Slice</Typography>
@@ -67,13 +84,16 @@ const Review = () => {
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Donec placerat, lectus sed mattis semper, neque lectus feugiat lectus,
-            varius pulvinar diam eros in elit. Pellentesque convallis laoreet
-            laoreet.
+            <TextField id="outlined-basic" label="Write a review" variant="outlined" sx={{ width: "100%" }} />
           </Typography>
+
         </AccordionDetails>
       </Accordion>
+
+      <Button variant="contained">Contained</Button>
     </Container >
+
+
   )
 }
 
