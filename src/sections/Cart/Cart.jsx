0d8e6f85d848e5c42438f7be206c4463527d56bb
Aileen Olitoquit <108008563+ayaolitoquit@users.noreply.github.com>
2023-12-24 14:39:19 +0800

    [MMA-19] Built Cart page

--- src/sections/Cart/Cart.jsx
+++ src/sections/Cart/Cart.jsx
@@ -1,11 +1,57 @@
+import React, { useState } from "react";
 import CartItemBlock from "./components/CartItemBlock";
 import CartPreviewBlock from "./components/CartPreviewBlock";
 
 export default function Cart() {
+  const [quantities, setQuantities] = useState({
+    1: 1,
+    2: 1,
+    3: 1,
+    4: 1,
+  });
+
+  const sampleCartItems = [
+    {
+      id: 1,
+      name: "Chocolate Obscura",
+      image: "/assets/images/products/1.png",
+      price: "154.00",
+      quantity: quantities[1] || 0,
+    },
+    {
+      id: 2,
+      name: "Biscuit Munch",
+      image: "/assets/images/products/2.png",
+      price: "154.00",
+      quantity: quantities[2] || 0,
+    },
+    {
+      id: 3,
+      name: "Alfredo Penne",
+      image: "/assets/images/products/3.png",
+      price: "154.00",
+      quantity: quantities[3] || 0,
+    },
+    {
+      id: 4,
+      name: "Cinnamon Rolls",
+      image: "/assets/images/products/4.png",
+      price: "154.00",
+      quantity: quantities[4] || 0,
+    },
+  ];
+
   return (
     <>
-      <CartItemBlock />
-      <CartPreviewBlock />
+      <CartItemBlock
+        sampleCartItems={sampleCartItems}
+        quantities={quantities}
+        setQuantities={setQuantities}
+      />
+      <CartPreviewBlock
+        sampleCartItems={sampleCartItems}
+        quantities={quantities}
+      />
     </>
-  )
+  );
 }
--- src/sections/Cart/components/CartItemBlock/CartItemBlock.jsx
+++ src/sections/Cart/components/CartItemBlock/CartItemBlock.jsx
@@ -1,4 +1,5 @@
-import { Stack,
+import { 
+  Stack,
   Typography,
   Grid, 
   Button,
@@ -6,55 +7,32 @@ import { Stack,
   Box,
   CardContent,
 } from "@mui/material"
-import { useState } from "react";
 import Iconify from "src/components/iconify";
+import { PropTypes } from "prop-types";
 
-const CartItemBlock = () => {
-  const [quantity, setQuantity] = useState(0);
-
-  const handleIncrement = () => {
-    setQuantity((prevQuantity) => prevQuantity + 1);
+const CartItemBlock = ({sampleCartItems, quantities, setQuantities}) => {
+  const handleIncrement = (productId) => {
+    setQuantities((prevQuantities) => ({
+      ...prevQuantities,
+      [productId]: (prevQuantities[productId] || 0) + 1,
+    }));
   };
 
-  const handleDecrement = () => {
-    if (quantity > 0) {
-      setQuantity((prevQuantity) => prevQuantity - 1);
+  const handleDecrement = (productId) => {
+    if (quantities[productId] > 0) {
+      setQuantities((prevQuantities) => ({
+        ...prevQuantities,
+        [productId]: prevQuantities[productId] - 1,
+      }));
     }
   };
 
-  const sampleCartItems = [
-    {
-      id: 1,
-      name: "Chocolate Obscura",
-      image: "/assets/images/products/1.png",
-      price: "154.00",
-    },
-    {
-      id: 2,
-      name: "Biscuit Munch",
-      image: "/assets/images/products/2.png",
-      price: "154.00",
-    },
-    {
-      id: 3,
-      name: "Alfredo Penne",
-      image: "/assets/images/products/3.png",
-      price: "154.00",
-    },
-    {
-      id: 4,
-      name: "Cinnamon Rolls",
-      image: "/assets/images/products/4.png",
-      price: "154.00",
-    },
-  ]
-
   return (
-    <Stack spacing={1}>
+    <Stack spacing={1} sx={{paddingBottom: 22 }}>
       {sampleCartItems.map((product) => (
 
         <Grid container spacing={1} key={product.id} >
-          <Grid item xs={3} >
+          <Grid item xs={6} sx={{display: "flex", flexDirection:"row"}}>
             <CardMedia
               component="img"
               sx={{ 
@@ -66,12 +44,12 @@ const CartItemBlock = () => {
               image={product.image}
               alt=""
             />
-          </Grid>
-          <Grid item xs={6}>
+          
             <Box 
               sx={{
                 display: "flex",
                 flexDirection: "column",
+                ml: 2
               }}>
               <CardContent 
                 sx={{ 
@@ -101,11 +79,11 @@ const CartItemBlock = () => {
                 alignItems: "center"
               }}>
               <Button
-                onClick={handleDecrement}
+                onClick={() => handleDecrement(product.id)}
                 sx={{
                   minWidth: "auto",
                   mr: 1,
-                  color: quantity === 0 ? "#B1B1B1" : "#333333"
+                  color: quantities[product.id] === 0 ? "#B1B1B1" : "#333333"
                 }}
               >
                 <Iconify icon="eva:minus-outline" />
@@ -120,10 +98,10 @@ const CartItemBlock = () => {
                   minWidth: "30px",
                   textAlign: "center"
                 }}>
-                {quantity}
+                {quantities[product.id] || 0}
               </Typography>
               <Button
-                onClick={handleIncrement}
+                onClick={() => handleIncrement(product.id)}
                 sx={{
                   minWidth: "auto",
                   ml: 1,
@@ -139,5 +117,10 @@ const CartItemBlock = () => {
     </Stack>
   )
 }
+CartItemBlock.propTypes = {
+  sampleCartItems: PropTypes.array.isRequired,
+  quantities: PropTypes.object.isRequired,
+  setQuantities: PropTypes.func.isRequired,
+};
 
 export default CartItemBlock
