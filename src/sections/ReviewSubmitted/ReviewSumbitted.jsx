1a5bbb942ad22f1d04d3c2ba5eb8d78870574943
marc <justineolimverio@gmail.com>
2023-12-27 14:17:02 +0800

    Update ReviewSumbitted.jsx

--- src/sections/ReviewSubmitted/ReviewSumbitted.jsx
+++ src/sections/ReviewSubmitted/ReviewSumbitted.jsx
@@ -1,19 +1,8 @@
-import {
-  Typography,
-  Box,
-
-} from "@mui/material"
+import { } from "@mui/material"
 
 const ReviewSubmitted = () => {
 
 
 }
-
-<Box sx={{ display: "flex", gap: "1em" }}>
-  <img src="/assets/images/checkmark.png" alt="" />
-  <Box>
-    <Typography sx={{ fontWeight: "bold" }}>Biscuit Munch</Typography>
-  </Box>
-</Box>
 
 export default ReviewSubmitted
