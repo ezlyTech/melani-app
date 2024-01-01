a9adae01463902d9251263fb19210a9466f04a4a
Eliz Marie Manalo <tech.ezly@gmail.com>
2024-01-01 12:52:13 +0800

    updated receipt styles

--- src/sections/Receipt/components/ReceiptBlock/ReceiptBlock.jsx
+++ src/sections/Receipt/components/ReceiptBlock/ReceiptBlock.jsx
@@ -13,7 +13,7 @@ import { useRouter } from "src/routes/hooks";
 const ReceiptBlock = () => {
   const store = "Melani's Bakehouse";
   const address = "30 Bayacal Street, Sabutan, Silang, Cavite, Philippines";
-  const orderNum = 69; 
+  const orderNum = 69;
   const customerName = "Soobin"
   const tableNum = 3;
 
@@ -21,40 +21,41 @@ const ReceiptBlock = () => {
     { name: "Double Choco Cake", price: 120.00 },
     { name: "Biscuit Munch", price: 146.00 },
   ];
-  
+
   const userDiscountRates = {
     "Double Choco Cake": 0.1, // 10%
     "Biscuit Munch": 0.1, // 10%
   };
-  
+
   const calculateItemTotal = (item) => {
     const discountRate = userDiscountRates[item.name] || 0;
     const itemDiscount = item.price * discountRate;
     const itemTotal = item.price - itemDiscount;
     return { original: item.price, discounted: itemTotal };
   };
-  
+
   const subtotal = items.reduce((acc, item) => acc + calculateItemTotal(item).original, 0);
   const total = items.reduce((acc, item) => acc + calculateItemTotal(item).discounted, 0);
-  
 
-  
+
+
   const router = useRouter();
   const redirectToReview = () => {
-    
+
     if (router.push) {
       router.push("/review");
     } else {
       router.push("/404")
     }
   };
-  
+
   return (
     <Container>
-      <Typography 
+      <Typography
         align="center"
-        sx={{mb: 1,
-          "& h4": {mb: 1}
+        sx={{
+          mb: 1,
+          "& h4": { mb: 1 }
         }}
       >
         <h4>{store}</h4>
@@ -63,28 +64,31 @@ const ReceiptBlock = () => {
 
       <Box
         sx={{
-          "& h5":{
+          "& h5": {
             margin: 1
           }
         }}
       >
         <Typography align="center"
-          sx={{ 
+          sx={{
             mb: 3,
-            "& h1": {mb: 0, mt: 2 }
+            "& h1": { mb: 0, mt: 2 }
           }}
         >
           <h1>{orderNum}</h1>
           Order Number
         </Typography>
-        <h5>Time & Date: 5:53 03/04/19</h5>
-        <h5>Table Number: {tableNum}</h5>
-        <h5>Customer: {customerName}</h5>
+        <Typography variant="caption" fontWeight={500}>Time & Date: 5:53 03/04/19</Typography> <br />
+        <Typography variant="caption" fontWeight={500}>Table Number: {tableNum}</Typography> <br />
+        {customerName && (
+          <Typography variant="caption" fontWeight={500}>Customer Name: {customerName}</Typography>
+        )}
       </Box>
 
 
-      <Box sx={{ 
+      <Box sx={{
         flexGrow: 2,
+        mt: 2,
         pb: 2,
         borderTop: 1,
         widht: "100%",
@@ -106,27 +110,27 @@ const ReceiptBlock = () => {
               <strong>
                 <p style={{ textAlign: "end" }}>₱{item.price.toFixed(2)}</p>
               </strong>
-              
+
             </Grid>
 
             {/* DISCOUNT */}
             <Grid item xs={8}>
-              <p  
-                style={{ 
-                  marginTop:-5,
+              <p
+                style={{
+                  marginTop: -5,
                   marginLeft: 10,
-                  marginBottom:2,
-                  color:"#979797"
+                  marginBottom: 2,
+                  color: "#979797"
                 }}>
                 Student Discount ({(userDiscountRates[item.name] * 100).toFixed(2)}%):
               </p>
             </Grid>
             <Grid item xs={4} >
-              <p style={{ 
-                textAlign: "end", 
-                marginTop:-5,
-                marginBottom:2,
-                color:"#979797"
+              <p style={{
+                textAlign: "end",
+                marginTop: -5,
+                marginBottom: 2,
+                color: "#979797"
               }}>
                 -₱{(item.price * userDiscountRates[item.name]).toFixed(2)}
               </p>
@@ -138,6 +142,7 @@ const ReceiptBlock = () => {
 
       <Box>
         <Typography
+          variant="subtitle2"
           sx={{
             display: "flex",
             flexDirection: "column",
@@ -145,7 +150,7 @@ const ReceiptBlock = () => {
             mb: 2,
             "& strong": {
               mt: 1,
-              ml:1,
+              ml: 1,
               display: "flex",
               justifyContent: "space-between",
               width: "95%",
@@ -167,19 +172,21 @@ const ReceiptBlock = () => {
           ))}
         </Typography>
 
-        <Box 
+        <Box
           sx={{
             borderTop: 1,
           }}>
-          <Typography variant="h3"
+          <Typography
+            variant="h5"
             sx={{
-              display: "flex", 
-              justifyContent: "space-between"
+              display: "flex",
+              justifyContent: "space-between",
+              mt: 1.5
             }}
           >
             <strong>Amount Due:</strong> ₱{total.toFixed(2)}
           </Typography>
-          
+
         </Box>
       </Box>
       <Button
@@ -191,12 +198,12 @@ const ReceiptBlock = () => {
           background: "#888C03",
           color: "#FFF",
           borderRadius: "30px",
-          mt:5,
+          mt: 5,
           mb: 2
         }}
         onClick={redirectToReview}
       >
-            Done
+        Done
       </Button>
     </Container>
   );
