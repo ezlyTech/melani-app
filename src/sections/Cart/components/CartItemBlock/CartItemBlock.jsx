a7c5c58360b8444fdb1e0a7b6028783021df67ea
Aileen Olitoquit <108008563+ayaolitoquit@users.noreply.github.com>
2024-01-04 23:53:28 +0800

    [MMA-21] removed voucher

--- src/sections/Cart/components/CartItemBlock/CartItemBlock.jsx
+++ src/sections/Cart/components/CartItemBlock/CartItemBlock.jsx
@@ -16,36 +16,8 @@ import {
 } from "@mui/material"
 import Iconify from "src/components/iconify";
 import { PropTypes } from "prop-types";
-import { useState } from "react";
 
 const CartItemBlock = ({ sampleCartItems, quantities, setQuantities, selectedSize, setSelectedSize, clearAddons }) => {
-  const [voucherDiscount, setVoucherDiscount] = useState(0);
-
-  const handleVoucherSelect = (event, value) => {
-    if (value) {
-      setVoucherDiscount(value.discount);
-    } else {
-      setVoucherDiscount(0);
-    }
-  };
-
-  const voucher = [
-    {
-      id: 1,
-      label: "Student",
-      discount: 0.125
-    },
-    {
-      id: 2,
-      label: "Person w/ Disabilities",
-      discount: 0.1
-    },
-    {
-      id: 3,
-      label: "Senior",
-      discount: 0.1
-    }
-  ]
     
   const handleIncrement = (productId) => {
     setQuantities((prevQuantities) => ({
@@ -63,7 +35,6 @@ const CartItemBlock = ({ sampleCartItems, quantities, setQuantities, selectedSiz
     }
   };
 
-  console.log(voucherDiscount)
   console.log(selectedSize)
   return (
     <Container>
@@ -76,163 +47,145 @@ const CartItemBlock = ({ sampleCartItems, quantities, setQuantities, selectedSiz
       </Typography>
       <Stack spacing={1} sx={{ paddingBottom: 22 }}>
         {sampleCartItems.map((product) => (
-          <>
-            <Grid container spacing={1} pb={1} key={product.id} width="100%">
-              <Grid item xs={9} sx={{ display: "flex" }}>
-                <CardMedia
-                  component="img"
-                  sx={{
-                    width: "100%",
-                    maxWidth: 70,
-                    height: 75,
-                    borderRadius: 1
-                  }}
-                  image={product.image}
-                  alt=""
-                />
+          <Grid container spacing={1} pb={1} key={product.id} width="100%">
+            <Grid item xs={9} sx={{ display: "flex" }}>
+              <CardMedia
+                component="img"
+                sx={{
+                  width: "100%",
+                  maxWidth: 70,
+                  height: 75,
+                  borderRadius: 1
+                }}
+                image={product.image}
+                alt=""
+              />
 
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  ml: 2,
+                  gap: .5
+                }}>
+                <CardContent
+                  sx={{
+                    flex: "1 0 auto",
+                    padding: 0
+                  }}>
+                  <Typography
+                    gutterBottom
+                    variant="body2"
+                    mb={0}
+                    sx={{
+                      fontWeight: 400
+                    }}>
+                    {product.name}
+                  </Typography>
+                  <Typography variant="caption" color="text.secondary">
+                    ₱ {product.price}
+                  </Typography>
+                </CardContent>
+                <FormControl 
+                  sx={{ 
+                    width:  "fit-content",
+                    minWidth: 80, 
+                    zIndex:0, 
+                    height: "fit-content",
+                    "& .MuiOutlinedInput-input": {
+                      fontSize: 12,
+                    },
+                    "&. MuiFormControl": {
+                      height: "5px",
+                      width: 100
+                    }
+                  }} 
+                  size="small">
+                  <InputLabel id="demo-select-small-label">
+                    Size
+                  </InputLabel>
+                  <Select
+                    labelId="demo-select-small-label"
+                    id="demo-select-small"
+                    value={selectedSize}
+                    label="Size"
+                    onChange={(event) => setSelectedSize(event.target.value)}
+                  >
+                    <MenuItem value="default">
+                      <em>None</em>
+                    </MenuItem>
+                    <MenuItem value="16oz">16oz</MenuItem>
+                    <MenuItem value="24oz">24oz</MenuItem>
+                  </Select>
+                </FormControl>
+                <Chip 
+                  label="Expresso Shot" 
+                  variant="outlined" 
+                  onDelete={
+                    <IconButton size="small">{clearAddons}
+                    </IconButton>} 
+                  sx={{width: "fit-content", height:30}} 
+                />
+                <Chip 
+                  label="Milk" 
+                  variant="outlined" 
+                  onDelete={
+                    <IconButton size="small">{clearAddons}
+                    </IconButton>} 
+                  sx={{width: "fit-content", height:30}} 
+                />
+              </Box>
+            </Grid>
+
+            <Grid item xs={3}>
+              <Container>
                 <Box
                   sx={{
                     display: "flex",
-                    flexDirection: "column",
-                    ml: 2,
-                    gap: .5
+                    justifyContent: "center",
+                    alignItems: "center",
+                    width: "100%"
                   }}>
-                  <CardContent
+                  <Button
+                    onClick={() => handleDecrement(product.id)}
                     sx={{
-                      flex: "1 0 auto",
-                      padding: 0
+                      minWidth: "auto",
+                      mr: 0.5,
+                      color: quantities[product.id] === 0 ? "#B1B1B1" : "#333333"
+                    }}
+                  >
+                    <Iconify icon="eva:minus-outline" width="14px" />
+                  </Button>
+                  <Typography
+                    sx={{
+                      background: "#FFF2ED",
+                      color: "#DDA15E",
+                      p: ".5em 1em .5em 1em",
+                      borderRadius: "4px",
+                      fontWeight: 600,
+                      minWidth: "30px",
+                      textAlign: "center",
+                      fontSize: ".70rem"
                     }}>
-                    <Typography
-                      gutterBottom
-                      variant="body2"
-                      mb={0}
-                      sx={{
-                        fontWeight: 400
-                      }}>
-                      {product.name}
-                    </Typography>
-                    <Typography variant="caption" color="text.secondary">
-                      ₱ {product.price}
-                    </Typography>
-                  </CardContent>
-                  <FormControl 
-                    sx={{ 
-                      width:  "fit-content",
-                      minWidth: 80, 
-                      fontSize: "1rem", 
-                      zIndex:0, 
-                      height: "auto",
-                      // p: .1,
-                      "& .MuiOutlinedInput-input": {
-                        fontSize: 12,
-                      }
-                    }} 
-                    size="small">
-                    <InputLabel id="demo-select-small-label">Size</InputLabel>
-                    <Select
-                      labelId="demo-select-small-label"
-                      id="demo-select-small"
-                      value={selectedSize}
-                      label="Size"
-                      onChange={(event) => setSelectedSize(event.target.value)}
-                    >
-                      <MenuItem value="default">
-                        <em>None</em>
-                      </MenuItem>
-                      <MenuItem value="16oz">16oz</MenuItem>
-                      <MenuItem value="24oz">24oz</MenuItem>
-                    </Select>
-                  </FormControl>
-                  <Chip label="Expresso Shot" variant="outlined" onDelete={<IconButton size="small">{clearAddons}</IconButton>} sx={{width: "fit-content", height:20}} />
-                  <Chip label="Milk" variant="outlined" onDelete={<IconButton size="small">{clearAddons}</IconButton>} sx={{width: "fit-content", height:20}} />
+                    {quantities[product.id] || 0}
+                  </Typography>
+                  <Button
+                    onClick={() => handleIncrement(product.id)}
+                    sx={{
+                      minWidth: "auto",
+                      ml: 0.5,
+                      color: "#333333"
+                    }}
+                  >
+                    <Iconify icon="eva:plus-outline" width="14px" sx={{"&.MuiButton": {pl:"500px"}}} />
+                  </Button>
                 </Box>
-              </Grid>
-
-              <Grid item xs={3}>
-                <Container>
-                  <Box
-                    sx={{
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center",
-                      width: "100%"
-                    }}>
-                    <Button
-                      onClick={() => handleDecrement(product.id)}
-                      sx={{
-                        minWidth: "auto",
-                        mr: 0.5,
-                        color: quantities[product.id] === 0 ? "#B1B1B1" : "#333333"
-                      }}
-                    >
-                      <Iconify icon="eva:minus-outline" width="14px" />
-                    </Button>
-                    <Typography
-                      sx={{
-                        background: "#FFF2ED",
-                        color: "#DDA15E",
-                        p: ".5em 1em .5em 1em",
-                        borderRadius: "4px",
-                        fontWeight: 600,
-                        minWidth: "30px",
-                        textAlign: "center",
-                        fontSize: ".70rem"
-                      }}>
-                      {quantities[product.id] || 0}
-                    </Typography>
-                    <Button
-                      onClick={() => handleIncrement(product.id)}
-                      sx={{
-                        minWidth: "auto",
-                        ml: 0.5,
-                        color: "#333333"
-                      }}
-                    >
-                      <Iconify icon="eva:plus-outline" width="14px" sx={{"&.MuiButton": {pl:"500px"}}} />
-                    </Button>
-                  </Box>
-                  <IconButton>
-                    <Iconify icon="ic:outline-delete" color="#760101" />
-                  </IconButton>
-                </Container>
-              </Grid>
+                <IconButton>
+                  <Iconify icon="ic:outline-delete" color="#760101" />
+                </IconButton>
+              </Container>
             </Grid>
-            <FormControl 
-              sx={{ 
-                m: 1,
-                minWidth: 120
-              }} 
-              size="small">
-              <InputLabel 
-                id="demo-select-small-label" 
-                sx={{ 
-                  backgroundColor: "white", 
-                  p: "0 5px", 
-                  zIndex: 0,
-                  "&.MuiInputLabel-root.Mui-focused": { 
-                    backgroundColor: "white",
-                    zIndex: 1 
-                  },
-                }}>
-                  Select Discount
-              </InputLabel>
-              <Select
-                labelId="demo-select-small-label"
-                id="demo-select-small"
-                value={voucher}
-                label="Discount"
-                onChange={handleVoucherSelect}
-              >
-                <MenuItem value="default"><em>None</em></MenuItem>
-                {voucher.map((item) => (
-                  <MenuItem key={item.id} value={item.discount}>
-                    {item.label}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-          </>
+          </Grid>
         ))}
       </Stack>
     </Container>
