27b12260c4f7909b8817473ae0957acad72f1016
Aileen Olitoquit <108008563+ayaolitoquit@users.noreply.github.com>
2024-01-07 18:57:11 +0800

    [MMA-21] added variations

--- src/sections/Cart/components/CartItemBlock/CartItemBlock.jsx
+++ src/sections/Cart/components/CartItemBlock/CartItemBlock.jsx
@@ -5,9 +5,7 @@ import {
   Button,
   CardMedia,
   Box,
-  CardContent,
   Container,
-  IconButton,
   FormControl,
   InputLabel,
   Select,
@@ -16,9 +14,20 @@ import {
 } from "@mui/material"
 import Iconify from "src/components/iconify";
 import { PropTypes } from "prop-types";
+import { useState } from "react";
 
-const CartItemBlock = ({ sampleCartItems, quantities, setQuantities, selectedSize, setSelectedSize, clearAddons }) => {
+const CartItemBlock = ({ sampleCartItems, quantities, setQuantities, clearAddons, selectedAddons, selectedVariation }) => {
     
+  const [selectedSize, setSelectedSizes] = useState({});
+
+  const handleSize = (event, productId) => {
+    const newSize = event.target.value;
+
+    setSelectedSizes((prevSelectedSizes) => ({
+      ...prevSelectedSizes,
+      [productId]: newSize,
+    }));
+  };
   const handleIncrement = (productId) => {
     setQuantities((prevQuantities) => ({
       ...prevQuantities,
@@ -35,7 +44,6 @@ const CartItemBlock = ({ sampleCartItems, quantities, setQuantities, selectedSiz
     }
   };
 
-  console.log(selectedSize)
   return (
     <Container>
       <Typography 
@@ -53,8 +61,8 @@ const CartItemBlock = ({ sampleCartItems, quantities, setQuantities, selectedSiz
                 component="img"
                 sx={{
                   width: "100%",
-                  maxWidth: 70,
-                  height: 75,
+                  maxWidth: 85,
+                  height: 90,
                   borderRadius: 1
                 }}
                 image={product.image}
@@ -68,122 +76,141 @@ const CartItemBlock = ({ sampleCartItems, quantities, setQuantities, selectedSiz
                   ml: 2,
                   gap: .5
                 }}>
-                <CardContent
+                <Typography
+                  gutterBottom
+                  variant="body2"
+                  mb={0}
                   sx={{
-                    flex: "1 0 auto",
-                    padding: 0
+                    fontWeight: 400
                   }}>
-                  <Typography
-                    gutterBottom
-                    variant="body2"
-                    mb={0}
-                    sx={{
-                      fontWeight: 400
-                    }}>
-                    {product.name}
-                  </Typography>
-                  <Typography variant="caption" color="text.secondary">
-                    ₱ {product.price}
-                  </Typography>
-                </CardContent>
+                  {product.name}
+                </Typography>
+
                 <FormControl 
+                  size="small"
                   sx={{ 
-                    width:  "fit-content",
+                    width: "fit-content",
                     minWidth: 80, 
-                    zIndex:0, 
-                    height: "fit-content",
-                    "& .MuiOutlinedInput-input": {
+                    zIndex: 0, 
+                    "& .MuiSelect-select": {
                       fontSize: 12,
+                      p: "0.5rem"
                     },
-                    "&. MuiFormControl": {
-                      height: "5px",
-                      width: 100
+                    "& .MuiInputBase-root": {
+                      height: 25
                     }
-                  }} 
-                  size="small">
-                  <InputLabel id="demo-select-small-label">
+                  }}
+                >
+                  <InputLabel 
+                    id="demo-select-small-label"
+                    sx={{ fontSize: 12}}>
                     Size
                   </InputLabel>
                   <Select
                     labelId="demo-select-small-label"
                     id="demo-select-small"
-                    value={selectedSize}
+                    value={selectedSize[product.id] || "default"}
                     label="Size"
-                    onChange={(event) => setSelectedSize(event.target.value)}
+                    onChange={(event) => handleSize(event, product.id)}
                   >
                     <MenuItem value="default">
-                      <em>None</em>
+                      <em>16oz</em>
                     </MenuItem>
-                    <MenuItem value="16oz">16oz</MenuItem>
                     <MenuItem value="24oz">24oz</MenuItem>
                   </Select>
                 </FormControl>
+
+                {/* {selectedAddons && ( */}
                 <Chip 
                   label="Expresso Shot" 
                   variant="outlined" 
-                  onDelete={
-                    <IconButton size="small">{clearAddons}
-                    </IconButton>} 
-                  sx={{width: "fit-content", height:30}} 
+                  onDelete={() => clearAddons()} 
+                  sx={{
+                    width: "fit-content", 
+                    height:20,
+                    fontSize: 12,
+                    "& .MuiChip-deleteIcon": {
+                      fontSize: 15,
+                    }
+                  }} 
                 />
+                {/* )} */}
+                
+                {/* {selectedVariation && ( */}
                 <Chip 
                   label="Milk" 
                   variant="outlined" 
-                  onDelete={
-                    <IconButton size="small">{clearAddons}
-                    </IconButton>} 
-                  sx={{width: "fit-content", height:30}} 
+                  onDelete={{clearAddons}} 
+                  sx={{
+                    width: "fit-content",
+                    height:20,
+                    fontSize: 12,
+                    "& .MuiChip-deleteIcon": {
+                      fontSize: 15,
+                    }
+                  }} 
                 />
+                {/* )}  */}
+                <Typography 
+                  variant="caption" 
+                  color="primary.main"
+                  sx={{pt: 0.5, fontSize: 15}}
+                >
+                  ₱ {product.price}
+                </Typography>
               </Box>
             </Grid>
 
-            <Grid item xs={3}>
-              <Container>
-                <Box
+            <Grid item xs={3} 
+              sx={{
+                display:"flex",
+                flexDirection: "column",
+                justifyContent: "space-between"
+              }}>
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  width: "100%"
+                }}>
+                <Button
+                  onClick={() => handleDecrement(product.id)}
                   sx={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    width: "100%"
+                    minWidth: "auto",
+                    mr: 0.5,
+                    color: quantities[product.id] === 0 ? "#B1B1B1" : "#333333"
+                  }}
+                >
+                  <Iconify icon="eva:minus-outline" width="14px" />
+                </Button>
+                <Typography
+                  sx={{
+                    background: "#FFF2ED",
+                    color: "#DDA15E",
+                    p: ".5em 1em .5em 1em",
+                    borderRadius: "4px",
+                    fontWeight: 600,
+                    minWidth: "30px",
+                    textAlign: "center",
+                    fontSize: ".70rem"
                   }}>
-                  <Button
-                    onClick={() => handleDecrement(product.id)}
-                    sx={{
-                      minWidth: "auto",
-                      mr: 0.5,
-                      color: quantities[product.id] === 0 ? "#B1B1B1" : "#333333"
-                    }}
-                  >
-                    <Iconify icon="eva:minus-outline" width="14px" />
-                  </Button>
-                  <Typography
-                    sx={{
-                      background: "#FFF2ED",
-                      color: "#DDA15E",
-                      p: ".5em 1em .5em 1em",
-                      borderRadius: "4px",
-                      fontWeight: 600,
-                      minWidth: "30px",
-                      textAlign: "center",
-                      fontSize: ".70rem"
-                    }}>
-                    {quantities[product.id] || 0}
-                  </Typography>
-                  <Button
-                    onClick={() => handleIncrement(product.id)}
-                    sx={{
-                      minWidth: "auto",
-                      ml: 0.5,
-                      color: "#333333"
-                    }}
-                  >
-                    <Iconify icon="eva:plus-outline" width="14px" sx={{"&.MuiButton": {pl:"500px"}}} />
-                  </Button>
-                </Box>
-                <IconButton>
-                  <Iconify icon="ic:outline-delete" color="#760101" />
-                </IconButton>
-              </Container>
+                  {quantities[product.id] || 0}
+                </Typography>
+                <Button
+                  onClick={() => handleIncrement(product.id)}
+                  sx={{
+                    minWidth: "auto",
+                    ml: 0.5,
+                    color: "#333333",
+                  }}
+                >
+                  <Iconify icon="eva:plus-outline" width="14px" sx={{"&.MuiButton": {pl:"500px"}}} />
+                </Button>
+              </Box>
+              <Button sx={{p:0, justifyContent: "right"}}>
+                <Iconify icon="ic:outline-delete" color="#760101" />
+              </Button>
             </Grid>
           </Grid>
         ))}
@@ -195,9 +222,9 @@ CartItemBlock.propTypes = {
   sampleCartItems: PropTypes.array.isRequired,
   quantities: PropTypes.object.isRequired,
   setQuantities: PropTypes.func.isRequired,
-  selectedSize: PropTypes.string.isRequired,
-  setSelectedSize: PropTypes.func.isRequired,
   clearAddons: PropTypes.func.isRequired,
+  selectedAddons: PropTypes.object.isRequired,
+  selectedVariation: PropTypes.object.isRequired,
 };
 
 export default CartItemBlock
