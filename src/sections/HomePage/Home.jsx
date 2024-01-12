7cb706c6b540f29358d2b154f222ca6f3138e89f
jmagwili <joshuamagwili@gmail.com>
2024-01-12 11:58:41 +0800

    Made pages render customer name from session storage

--- src/sections/HomePage/Home.jsx
+++ src/sections/HomePage/Home.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import {
   Card,
   CardMedia,
@@ -8,18 +8,21 @@ import {
 import axios from "axios";
 import { useAuth0 } from "@auth0/auth0-react";
 import usePreventReload from "src/routes/hooks/usePreventReload";
-import UserContext from "../../UserContext";
+// import UserContext from "../../UserContext";
 import { HomeCategoriesBlock, HomeMenuBlock } from "./components";
 
 export default function Home() {
   const { user, isAuthenticated } = useAuth0()
-  const { name } = useContext(UserContext);
+  // const { name } = useContext(UserContext);
+  const [name, setName] = useState()
   const [categories, setCategories] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   usePreventReload()
 
   useEffect(() => {
+    setName(sessionStorage.getItem("username"))
+
     const fetchData = async () => {
       try {
         const itemData = await axios.get("http://localhost:3031/api/categories");
