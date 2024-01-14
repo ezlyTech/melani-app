4881f76f300f2e2b83080eb34ae8985b7315c4d9
jmagwili <joshuamagwili@gmail.com>
2024-01-14 20:49:15 +0800

    added logic to get storeID and paymentID at server's orderRoute get method

--- server/routes/orderRoute.js
+++ server/routes/orderRoute.js
@@ -3,8 +3,30 @@ import axios from "axios"
 
 const orderRoute = express.Router()
 
-orderRoute.get("/", (req, res) => {
-  res.send("test")
+orderRoute.post("/", async (req, res) => {
+  try {
+    const store = await axios.get('https://api.loyverse.com/v1.0/stores/', {
+      headers: {
+        'Authorization': `Bearer ${process.env.VITE_LOYVERSE_TOKEN}`
+      }
+    })
+    const paymentType = await axios.get('https://api.loyverse.com/v1.0/payment_types/', {
+      headers: {
+        'Authorization': `Bearer ${process.env.VITE_LOYVERSE_TOKEN}`
+      }
+    })
+    const storeID = store.data.stores[0].id
+
+    const requestBody = {
+      store_id: storeID,
+      line_items: req.body.lineItems,
+    }
+
+    res.send(paymentType.data.payment_types.find((payment) => payment.type === 'CASH').id)
+
+  } catch (err) {
+    console.log(err)
+  }
 })
 
 export default orderRoute
