e58fc600c73bffc81dab6ba823147492a1a167f2
jmagwili <joshuamagwili@gmail.com>
2024-01-15 11:30:58 +0800

    added logic to review route to send data of items to be reviewed to client

--- server/routes/reviewsRoute.js
+++ server/routes/reviewsRoute.js
@@ -3,8 +3,38 @@ import axios from 'axios'
 
 const reviewsRoute = express.Router()
 
-reviewsRoute.get('/', (req, res) => {
-  res.send('test')
+reviewsRoute.get('/:receiptNo', async (req, res) => {
+  try {
+    const receipt = await axios.get(`https://api.loyverse.com/v1.0/receipts/${req.params.receiptNo}`, {
+      headers: {
+        'Authorization': `Bearer ${process.env.VITE_LOYVERSE_TOKEN}`
+      }
+    });
+
+    const itemIDs = receipt.data.line_items.map(item => item.item_id)
+
+    const itemRequests = itemIDs.map(async (id) => {
+      const item = await axios.get(`https://api.loyverse.com/v1.0/items/${id}`, {
+        headers: {
+          'Authorization': `Bearer ${process.env.VITE_LOYVERSE_TOKEN}`
+        }
+      })
+      return {
+        id: item.data.id,
+        name: item.data.item_name,
+        image: item.data.image_url
+      }
+    })
+
+    const itemList = await Promise.all(itemRequests)
+
+    res.send(itemList)
+
+  } catch (err) {
+    console.log(err)
+    res.send({ message: 'Server responded with an error' })
+  }
 })
+
 
 export default reviewsRoute
