6ff445ac068b28551e458fd7300b67a5e6b3f1cf
Eliz Marie Manalo <tech.ezly@gmail.com>
2024-01-21 21:34:28 +0800

    removed rate us in nav

--- src/sections/Favorites/Favorites.jsx
+++ src/sections/Favorites/Favorites.jsx
@@ -12,25 +12,25 @@ const Favorites = () => {
     {
       name: "Chocolate Obscura",
       image: "/assets/images/products/1.png",
-      price: "154.00",
+      price: "154",
       rating: 4,
     },
     {
       name: "Biscuit Munch",
       image: "/assets/images/products/2.png",
-      price: "154.00",
+      price: "154",
       rating: 3,
     },
     {
       name: "Alfredo Penne",
       image: "/assets/images/products/3.png",
-      price: "154.00",
+      price: "154",
       rating: 5,
     },
     {
       name: "Cinnamon Rolls",
       image: "/assets/images/products/4.png",
-      price: "154.00",
+      price: "154",
       rating: 5,
     },
   ];
@@ -63,7 +63,7 @@ const Favorites = () => {
         </Grid>
       </Container>
     )
-    
+
   );
 };
 
